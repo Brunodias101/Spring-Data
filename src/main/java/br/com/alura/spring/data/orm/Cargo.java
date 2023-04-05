537dedadb32bd8deb37d3df47ea72a7b60f28264
brunodias101 <Br@22356uno>
2023-04-04 21:13:26 -0300

    Subindo o projeto com a primeira entidade criada

---
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -0,0 +1,29 @@
+package br.com.alura.spring.data.orm;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/test/java/br/com/alura/spring/data/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
